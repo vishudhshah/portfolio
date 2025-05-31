cddae8f7a6161cf553908b790d7d17641429cced
Vishudh Shah <vishudh@hotmail.com>
2025-05-30 22:29:52 -0700

    2.1

--- meta/meta.js
+++ meta/meta.js
@@ -368,6 +368,30 @@ function renderLanguageBreakdown(selection, commits) {
     }
 }
 
+function updateFileDisplay(filteredCommits) {
+    let lines = filteredCommits.flatMap(d => d.lines);
+    let files = d3
+        .groups(lines, d => d.file)
+        .map(([name, lines]) => {
+            return { name, lines };
+        })
+        .sort((a, b) => b.lines.length - a.lines.length); // Sorting added for consistency with Step 2.3
+
+    let filesContainer = d3
+        .select('#files')
+        .selectAll('div')
+        .data(files, d => d.name)
+        .join(
+            (enter) =>
+                enter.append('div').call((div) => {
+                    div.append('dt').append('code');
+                    div.append('dd');
+                })
+        );
+    filesContainer.select('dt > code').text(d => d.name);
+    filesContainer.select('dd').text(d => `${d.lines.length} lines`);
+}
+
 function onTimeSliderChange() {
     const slider = document.getElementById("commit-progress");
     commitProgress = +slider.value;
@@ -381,6 +405,7 @@ function onTimeSliderChange() {
 
     filteredCommits = commits.filter(d => d.datetime <= commitMaxTime);
     updateScatterPlot(data, filteredCommits);
+    updateFileDisplay(filteredCommits);
 }
 
 
